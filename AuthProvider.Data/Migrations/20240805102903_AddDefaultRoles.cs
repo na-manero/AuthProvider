aff30053e993df139ee1999f7c7464484affc061
Chris <christofferjcm@gmail.com>
2024-08-06 13:21:23 +0200

    Added default roles

---
+++ AuthProvider.Data/Migrations/20240805102903_AddDefaultRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthProvider.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO AspNetRoles (Id, Name, NormalizedName, ConcurrencyStamp) VALUES (NEWID(), 'Admin', 'ADMIN', NEWID());
+                INSERT INTO AspNetRoles (Id, Name, NormalizedName, ConcurrencyStamp) VALUES (NEWID(), 'Manager', 'MANAGER', NEWID());
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM AspNetRoles WHERE NormalizedName IN ('ADMIN', 'MANAGER');");
+        }
+    }
+}
